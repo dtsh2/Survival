b8ec911203d41142ef71ad1c65e6aeb112ac555a
David Hayman <davidtshayman@gmail.com>
2016-03-09 10:14:46 +1300

    commit all non-code files
    lots of details, none important

--- initial_cond.R
+++ initial_cond.R
@@ -51,3 +51,16 @@ list(a=rnorm(1,30),a1=rnorm(1,0.1),a2=rnorm(1,0.1),a3=rnorm(1,-0.01),b1=rnorm(1,
 # DarEsSalaam
 list(a=rnorm(1,10),a1=rnorm(1,0.1),a2=rnorm(1,0.1),a3=rnorm(1,-0.01),b1=rnorm(1,-0.01),b3=rnorm(1,-10),data.sd=runif(1,1,30))))
 colnames(both_init)<-names_c
+
+sen_mat_init<-as.data.frame(cbind(
+  # Ghana 
+  list(a=rnorm(1,300),a1=rnorm(1,5),b1=rnorm(1,7),data.sd=runif(1,0,30)),
+  # Sao Tome
+  list(a=rnorm(1,30),a1=rnorm(1,5),b1=rnorm(1,7),data.sd=runif(1,0,30)),
+  # Principe
+  list(a=rnorm(1,20),a1=rnorm(1,0.1),b1=rnorm(1,0.1),data.sd=runif(1,0,30)),
+  # Morogoro 
+  list(a=rnorm(1,14),a1=rnorm(1,0.1),b1=rnorm(1,0.1),data.sd=runif(1,0,30)),
+  # DarEsSalaam
+  list(a=rnorm(1,18),a1=rnorm(1,0.5),b1=rnorm(1,0.5),data.sd=runif(1,0,30))))
+colnames(sen_mat_init)<-names_c
